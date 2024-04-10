9191307ae8a38b184df3cad3738c60dce09245b5
sahilg80 <38989332+sahilg80@users.noreply.github.com>
2024-04-11 02:22:25 +0530

    added generic state machine

---
+++ Assets/Scripts/Utilities/GenericStateMachine.cs
@@ -0,0 +1,38 @@
+﻿using StatePattern.Enemy;
+using StatePattern.StateMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Utilities
+{
+    public class GenericStateMachine<T> where T : EnemyController
+    {
+        protected T Owner;
+        protected IState currentState;
+        protected Dictionary<States, IState> States = new Dictionary<States, IState>();
+
+        public GenericStateMachine(T Owner) => this.Owner = Owner;
+
+        public void Update() => currentState?.Update();
+
+        protected void ChangeState(IState newState)
+        {
+            currentState?.OnStateExit();
+            currentState = newState;
+            currentState?.OnStateEnter();
+        }
+
+        public void ChangeState(States newState) => ChangeState(States[newState]);
+
+        protected void SetOwner()
+        {
+            foreach (IState state in States.Values)
+            {
+                state.Owner = Owner;
+            }
+        }
+    }
+}
